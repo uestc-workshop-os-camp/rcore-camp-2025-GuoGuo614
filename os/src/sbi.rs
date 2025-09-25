d6eb70ab1ad5ec38cb0beb3bef09277b0e913721
GuoGuo <2251270969@qq.com>
2025-09-25 11:50:28 +0800

    ch1 by hand-coding #1

---
+++ os/src/sbi.rs
@@ -0,0 +1,15 @@
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let mut ret;
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+        )
+    }
+}
+
+const SBI_SHUTDOWN: usize = 8;
+
+pub fn shutdown() -> ! {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    panic!("It should shutdown!");
+}
