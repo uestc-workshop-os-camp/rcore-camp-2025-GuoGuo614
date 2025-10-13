1c1b7533c9c2a594dca2d5d77efd924d2dd9d347
GuoGuo <2251270969@qq.com>
2025-10-13 19:32:05 +0800

    Try new BIG_STRIDE

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -9,7 +9,7 @@ use alloc::sync::{Arc, Weak};
 use alloc::vec::Vec;
 use core::cell::RefMut;
 
-pub const BIG_STRIDE: usize = 100_000;
+pub const BIG_STRIDE: usize = 1_000_000;
 
 /// Task control block structure
 ///
